c8c0b42f351dd129e93da6a4e711031efe59de07
Interweave05 <interweave@qq.com>
2024-11-02 15:40:31 +0800

    feat: 发布灰度图像

---
+++ src/camera/src/gray_node.cpp
@@ -0,0 +1,45 @@
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+
+
+class gray_node: public rclcpp::Node {
+public:
+	gray_node(): Node("gray_node") {
+		publisher_ =
+		    this->create_publisher<sensor_msgs::msg::Image>(
+		        "camera/gray_stream", 10);
+		timer_ = this->create_wall_timer(
+		    std::chrono::milliseconds(1),
+		    std::bind(&gray_node::publish_image, this));
+		cap_.open(0);
+	}
+
+private:
+	void publish_image() {
+		cv::Mat frame;
+		cap_ >> frame;
+		cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+		if(!frame.empty()) {
+			auto msg =
+			    cv_bridge::CvImage(std_msgs::msg::Header(),
+			                       "mono8", frame)
+			        .toImageMsg();
+			publisher_->publish(*msg);
+		}
+	}
+
+	cv::VideoCapture cap_;
+	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr
+	    publisher_;
+	rclcpp::TimerBase::SharedPtr timer_;
+};
+
+
+int main(int argc, char **argv) {
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<gray_node>());
+	rclcpp::shutdown();
+	return 0;
+}
