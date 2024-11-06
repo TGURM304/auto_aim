f580e7481cf41da6dbb96bc6ebe531e3c98cbb45
EiEddie <eieddie4u@outlook.com>
2024-11-06 22:34:03 +0800

    style: 精简命名空间, 修改变量命名

--- src/camera/src/video_node.cpp
+++ src/camera/src/video_node.cpp
@@ -1,22 +1,26 @@
+#include <chrono>
+
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 
+using Image = sensor_msgs::msg::Image;
+using namespace std::literals;
+
+
 class StreamNode: public rclcpp::Node {
 public:
 	StreamNode(): Node("stream_node") {
-		publisher_ =
-		    this->create_publisher<sensor_msgs::msg::Image>(
-		        "camera/stream", 10);
+		publisher_ = this->create_publisher<Image>(
+		    "camera/stream", 10);
 		timer_ = this->create_wall_timer(
-		    std::chrono::milliseconds(
-		        25), // 设置为适当的帧率
-		    std::bind(&StreamNode::publish, this));
+		    25ms, std::bind(&StreamNode::publish, this));
 
 		// 打开视频文件
-		cap.open("./1.mp4");
-		if(!cap.isOpened()) {
+		// TODO: 通过宏传入视频文件地址
+		cap_.open("./1.mp4");
+		if(!cap_.isOpened()) {
 			RCLCPP_ERROR(this->get_logger(),
 			             "Failed to open video file");
 			return;
@@ -24,29 +28,29 @@ public:
 
 		// 获取视频帧总数
 		total_frames_ = static_cast<int>(
-		    cap.get(cv::CAP_PROP_FRAME_COUNT));
+		    cap_.get(cv::CAP_PROP_FRAME_COUNT));
 		frame_counter_ = 0;
 	}
 
 private:
 	void publish() {
-		if(!cap.isOpened()) {
+		if(!cap_.isOpened()) {
 			RCLCPP_ERROR(this->get_logger(),
 			             "Video capture not initialized");
 			return;
 		}
 
 		cv::Mat frame;
-		cap >> frame; // 从视频流中获取下一帧
+		cap_ >> frame;
 
 		if(frame.empty()) {
-			// 如果帧为空，重置视频播放到开头
-			cap.set(cv::CAP_PROP_POS_FRAMES, 0);
+			// 如果帧为空, 重置视频播放到开头
+			cap_.set(cv::CAP_PROP_POS_FRAMES, 0);
 			frame_counter_ = 0;
-			cap >> frame; // 重新获取第一帧
+			cap_ >> frame;
 		}
 
-		// 将OpenCV Mat转换为ROS消息
+		// 将 OpenCV Mat 转为 ROS 消息
 		auto msg =
 		    cv_bridge::CvImage(std_msgs::msg::Header(),
 		                       "bgr8", frame)
@@ -56,14 +60,14 @@ private:
 		frame_counter_++;
 	}
 
-	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr
-	    publisher_;
+	rclcpp::Publisher<Image>::SharedPtr publisher_;
 	rclcpp::TimerBase::SharedPtr timer_;
 
-	cv::VideoCapture cap;
+	cv::VideoCapture cap_;
 	int frame_counter_;
 	int total_frames_;
 };
+
 
 int main(int argc, char **argv) {
 	rclcpp::init(argc, argv);
