cb3a59148f9f1b5f6ce8cea669d359b4c58b5f60
EiEddie <eieddie4u@outlook.com>
2024-11-06 22:03:41 +0800

    style!: 注释与代码风格
    抽离部分函数

--- src/camera/include/mindvision.hpp
+++ src/camera/include/mindvision.hpp
@@ -6,27 +6,40 @@
 #include <MVSDK/CameraApi.h>
 
 
-// TODO: 注释
 class MindVision {
 public:
 	MindVision();
 
 	~MindVision();
 
-	int Init(int channel);
+	/**
+	 * @brief 初始化相机
+	 *
+	 * @param channel : 1 为 MONO8, 其他为 BGR8
+	 * @return * int 相机描述符
+	 * @retval -1 没有连接设备
+	 * @retval -2 初始化失败
+	 */
+	int init(int channel);
 
-	cv::Mat getMindvision(int hCamera);
+	/**
+	 * @brief 获取帧
+	 *
+	 * @param camera 相机描述符
+	 */
+	cv::Mat getFrame(int camera);
 
 
 private:
-	// 处理后数据缓存区
-	unsigned char *g_pRgbBuffer;
-
-	int iCameraCounts = 1;
-	int iStatus = -1;
-	tSdkCameraDevInfo tCameraEnumList;
-	int hCamera;
-	tSdkCameraCapbility tCapability; // 设备描述信息
+	/// @brief 处理后数据缓存区
+	unsigned char *rgb_buffer_;
+	/// @brief 相机计数
+	int camera_cnt_ = 1;
+	int status_ = -1;
+	tSdkCameraDevInfo camera_enum_list_;
+	int camera_;
+	/// @brief 设备描述信息
+	tSdkCameraCapbility capability_;
 };
 
 #endif
--- src/camera/src/mindvision.cpp
+++ src/camera/src/mindvision.cpp
@@ -2,97 +2,105 @@
 
 
 MindVision::MindVision() {
-	printf("MindVision_Start");
+	printf("MindVision Start\n");
 }
 
 MindVision::~MindVision() {
 	// 反初始化相机
-	CameraUnInit(hCamera);
+	CameraUnInit(camera_);
 	// 释放分配的内存
-	free(g_pRgbBuffer);
+	free(rgb_buffer_);
 }
 
-int MindVision::Init(int channel = 3) {
+int MindVision::init(int channel = 3) {
 	CameraSdkInit(1);
 
-	// 枚举设备，并建立设备列表
-	iStatus = CameraEnumerateDevice(&tCameraEnumList,
-	                                &iCameraCounts);
-	printf("state = %d\n", iStatus);
-	printf("count = %d\n", iCameraCounts);
+	// 枚举设备, 并建立设备列表
+	status_ = CameraEnumerateDevice(&camera_enum_list_,
+	                                &camera_cnt_);
+	printf("state = %d\ncount = %d\n", status_,
+	       camera_cnt_);
 
 	// 没有连接设备
-	if(iCameraCounts == 0) {
+	if(camera_cnt_ == 0) {
 		return -1;
 	}
 
-	// 相机初始化。初始化成功后，才能调用任何其他相机相关的操作接口
-	iStatus =
-	    CameraInit(&tCameraEnumList, -1, -1, &hCamera);
+	// 相机初始化
+	// 初始化成功后, 才能调用任何其他相机相关的操作接口
+	status_ =
+	    CameraInit(&camera_enum_list_, -1, -1, &camera_);
 
 	// 初始化失败
-	printf("state = %d\n", iStatus);
-	if(iStatus != CAMERA_STATUS_SUCCESS) {
-		return -1;
+	printf("state = %d\n", status_);
+	if(status_ != CAMERA_STATUS_SUCCESS) {
+		return -2;
 	}
 
-	// 获得相机的特性描述结构体。该结构体中包含了相机可设置的各种参数的范围信息。决定了相关函数的参数
-	CameraGetCapability(hCamera, &tCapability);
+	// 获得相机的特性描述结构体
+	// 该结构体中包含了相机可设置的各种参数的范围信息, 决定了相关函数的参数
+	CameraGetCapability(camera_, &capability_);
 
-	// 分配内存，保存RGB数据
-	g_pRgbBuffer = (unsigned char *)malloc(
-	    tCapability.sResolutionRange.iHeightMax
-	    * tCapability.sResolutionRange.iWidthMax * 3);
+	// 分配内存, 保存RGB数据
+	rgb_buffer_ = (unsigned char*)malloc(
+	    capability_.sResolutionRange.iHeightMax
+	    * capability_.sResolutionRange.iWidthMax * 3);
 
-	// 让SDK进入工作模式，开始接收来自相机发送的图像数据
-	CameraPlay(hCamera);
+	// 进入工作模式, 开始接收来自相机发送的图像数据
+	CameraPlay(camera_);
 
 	// 配置相机输出格式
 	if(channel == 1) {
-		CameraSetIspOutFormat(hCamera,
+		CameraSetIspOutFormat(camera_,
 		                      CAMERA_MEDIA_TYPE_MONO8);
 	} else {
-		CameraSetIspOutFormat(hCamera,
+		CameraSetIspOutFormat(camera_,
 		                      CAMERA_MEDIA_TYPE_BGR8);
 	}
-	return hCamera;
+	return camera_;
 }
 
+void showText(cv::Mat& frame, const std::string& msg) {
+	printf("%s\n", msg.c_str());
+	cv::Point position(20, 240);
+	cv::putText(frame, msg, position,
+	            cv::FONT_HERSHEY_SIMPLEX, 1.0,
+	            cv::Scalar(0, 0, 255), 2);
+}
 
-cv::Mat MindVision::getMindvision(int hCamera) {
-	if(hCamera == -1) {
-		printf("相机初始化失败,Coding=-1\n");
+cv::Mat MindVision::getFrame(int camera) {
+	if(camera == -1) {
 		cv::Mat frame =
 		    cv::Mat::ones(480, 640, CV_8UC3) * 255;
-		std::string text =
-		    "Camera initialization failederror code: -1";
-		cv::Point position(20, 240);
-		cv::putText(frame, text, position,
-		            cv::FONT_HERSHEY_SIMPLEX, 1.0,
-		            cv::Scalar(0, 0, 255), 2);
+		showText(frame, "[-1] no device connected");
+		return frame;
+	} else if(camera == -2) {
+		cv::Mat frame =
+		    cv::Mat::ones(480, 640, CV_8UC3) * 255;
+		showText(frame,
+		         "[-2] camera initialization failed");
 		return frame;
 	} else {
-		tSdkFrameHead sFrameInfo;
-		BYTE *pbyBuffer;
+		tSdkFrameHead frame_info;
+		BYTE* buffer;
 
-		CameraGetImageBuffer(hCamera, &sFrameInfo,
-		                     &pbyBuffer, 1000);
-		CameraImageProcess(hCamera, pbyBuffer, g_pRgbBuffer,
-		                   &sFrameInfo);
+		CameraGetImageBuffer(camera, &frame_info, &buffer,
+		                     1000);
+		CameraImageProcess(camera, buffer, rgb_buffer_,
+		                   &frame_info);
 
 		// 使用 cv::Mat 替代 IplImage
-		cv::Mat frame(
-		    sFrameInfo.iHeight, // 高度
-		    sFrameInfo.iWidth,  // 宽度
-		    sFrameInfo.uiMediaType
-		            == CAMERA_MEDIA_TYPE_MONO8
-		        ? CV_8UC1
-		        : CV_8UC3, // 类型：单通道或者三通道
-		    g_pRgbBuffer // 数据缓冲区
-		);
+		cv::Mat frame(frame_info.iHeight, frame_info.iWidth,
+		              frame_info.uiMediaType
+		                      == CAMERA_MEDIA_TYPE_MONO8
+		                  /* 单通道或者三通道 */
+		                  ? CV_8UC1
+		                  : CV_8UC3,
+		              rgb_buffer_);
 
-		// 在成功调用CameraGetImageBuffer后，必须调用CameraReleaseImageBuffer来释放获得的buffer。
-		CameraReleaseImageBuffer(hCamera, pbyBuffer);
+		// 在成功调用 CameraGetImageBuffer 后,
+		// 必须调用 CameraReleaseImageBuffer 来释放获得的 buffer
+		CameraReleaseImageBuffer(camera, buffer);
 
 		return frame;
 	}
