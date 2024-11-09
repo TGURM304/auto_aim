26795b19a0ec82e611bb846e03910160cd2e231e
Interweave05 <interweave@qq.com>
2024-11-09 19:23:30 +0800

    fix: 修复hCamera参数传入错误

--- src/camera/src/stream_node.cpp
+++ src/camera/src/stream_node.cpp
@@ -5,42 +5,44 @@
 
 #include "mindvision.hpp"
 
-
 class stream_node: public rclcpp::Node {
 public:
-	stream_node(): Node("stream_node") {
-		publisher_ =
-		    this->create_publisher<sensor_msgs::msg::Image>(
-		        "camera/stream", 10);
-		timer_ = this->create_wall_timer(
-		    std::chrono::milliseconds(0),
-		    std::bind(&stream_node::publish, this));
-	}
+    stream_node(): Node("stream_node") {
+        
+          
+        if (hCamera < 0) {
+            RCLCPP_ERROR(this->get_logger(), "Camera initialization failed");
+        }
 
+        publisher_ =
+            this->create_publisher<sensor_msgs::msg::Image>("camera/stream", 10);
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(1),
+            std::bind(&stream_node::publish, this));
+    }
+	~stream_node(){
+		camera_.~MindVision();
+	}
 private:
-	void publish() {
-		int camera = camera_.init(2);
-		cv::Mat frame = camera_.getFrame(camera);
-		if(!frame.empty()) {
-			auto msg =
-			    cv_bridge::CvImage(std_msgs::msg::Header(),
-			                       "bgr8", frame)
-			        .toImageMsg();
-			publisher_->publish(*msg);
-		}
-	}
+    void publish() {
+        cv::Mat frame = camera_.getFrame(hCamera);
+        if (!frame.empty()) {
+            auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
+            publisher_->publish(*msg);
+        } else {
+            RCLCPP_WARN(this->get_logger(), "Empty frame received");
+        }
+    }
 
-	MindVision camera_;
-	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr
-	    publisher_;
-	rclcpp::TimerBase::SharedPtr timer_;
+    MindVision camera_;
+	int hCamera = camera_.init(2);
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
-// TODO: 声明与实现分离
-
 
 int main(int argc, char **argv) {
-	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<stream_node>());
-	rclcpp::shutdown();
-	return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<stream_node>());
+    rclcpp::shutdown();
+    return 0;
 }
