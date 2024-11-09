b77ad511b4db074fa5f9416e3c837a63bbaab776
EiEddie <eieddie4u@outlook.com>
2024-11-09 20:52:03 +0800

    chore: 删除无用代码

--- src/camera/dependencies/MVSDK/MVSDKConfig.cmake
+++ src/camera/dependencies/MVSDK/MVSDKConfig.cmake
@@ -1,31 +1,3 @@
-# # ament_export_targets(MVSDK HAS_LIBRARY_TARGET)
-# # # ament_export_dependencies(some_dependency)
-
-# # file(GLOB_RECURSE include_files "${CMAKE_CURRENT_SOURCE_DIR}/include/*")
-# # add_library(MVSDK include_files)
-
-# # install(
-# #   DIRECTORY include/
-# #   DESTINATION include
-# # )
-
-# # install(
-# #   TARGETS MVSDK
-# #   EXPORT MVSDK
-# #   LIBRARY DESTINATION lib
-# #   ARCHIVE DESTINATION lib
-# #   INCLUDES DESTINATION include
-# # )
-
-# target_include_directories(MVSDK
-#   PUBLIC
-#     "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>"
-#     "$<INSTALL_INTERFACE:include>")
-
-# install(
-#   DIRECTORY include/
-#   DESTINATION include
-# )
 # 设置包含路径和库路径
 set(MVSDK_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/include")
 set(MVSDK_LIB "${CMAKE_CURRENT_LIST_DIR}/lib/libMVSDK.so")
@@ -39,8 +11,3 @@ set_target_properties(MVSDK PROPERTIES
 
 # 设置给 find_package 的输出变量
 set(MVSDK_FOUND TRUE)
-
-# ament_export_include_directories("${MVSDK_DIR}/include")
-# ament_export_libraries(MVSDK)
-
-# ament_package()
