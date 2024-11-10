1375d09d9dbd850c8d3e4c656371dc971c8a028e
Interweave05 <interweave@qq.com>
2024-11-10 16:50:57 +0800

    chore: 测试视频录制

--- src/camera/src/test_node.cpp
+++ src/camera/src/test_node.cpp
@@ -1,24 +1,32 @@
-#include <cstdio>
+// #include <cstdio>
 
-#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
 
 #include "mindvision.hpp"
 
 
-int main() {
-	auto mv = MindVision();
-	mv.init(2);
+// int main() {
+// 	auto mv = MindVision();
+// 	mv.init(2);
 
-	while(1) {
-		cv::Mat frame = mv.getFrame();
-		if(!frame.empty()) {
-			// 显示图像
-			cv::imshow("Camera Frame", frame);
-			cv::waitKey(1);
-		} else {
-			printf("Failed to capture image.\n");
-			break;
-		}
-	}
+// 	while(1) {
+// 		cv::Mat frame = mv.getFrame();
+// 		if(!frame.empty()) {
+// 			// 显示图像
+// 			cv::imshow("Camera Frame", frame);
+// 			cv::waitKey(1);
+// 		} else {
+// 			printf("Failed to capture image.\n");
+// 			break;
+// 		}
+// 	}
+// 	return 0;
+// }
+
+
+int main() {
+	MindVision camera_;
+	camera_.init(2);
+	camera_.record("/home/interweave", 60000);
 	return 0;
 }
