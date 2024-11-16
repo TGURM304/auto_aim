b4c69fb9b452eef124ad7a04bfce9fa205c167f0
Interweave05 <interweave@qq.com>
2024-11-16 15:51:30 +0800

    chore: 无更改

--- src/camera/src/test_node.cpp
+++ src/camera/src/test_node.cpp
@@ -1,27 +1,30 @@
-// #include <cstdio>
+#include <cstdio>
 
-// #include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 #include "mindvision.hpp"
 
 
-// // int main() {
-// // 	auto mv = MindVision();
-// // 	mv.init(2);
+// int main() {
+// 	auto mv = MindVision();
+// 	mv.init(2);
 
-// // 	while(1) {
-// // 		cv::Mat frame = mv.getFrame();
-// // 		if(!frame.empty()) {
-// // 			// 显示图像
-// // 			cv::imshow("Camera Frame", frame);
-// // 			cv::waitKey(1);
-// // 		} else {
-// // 			printf("Failed to capture image.\n");
-// // 			break;
-// // 		}
-// // 	}
-// // 	return 0;
-// // }
+// 	while(1) {
+// 		cv::Mat frame = mv.getFrame();
+// 		if(!frame.empty()) {
+// 			cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+
+// 			cv::threshold(frame, frame, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+// 			// 显示图像
+// 			cv::imshow("Camera Frame", frame);
+// 			cv::waitKey(1);
+// 		} else {
+// 			printf("Failed to capture image.\n");
+// 			break;
+// 		}
+// 	}
+// 	return 0;
+// }
 
 
 int main() {
