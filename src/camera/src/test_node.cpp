22523ec267661cf7b70e54cf104c3f103dc850e9
Interweave05 <interweave@qq.com>
2024-11-16 14:58:46 +0800

    chore: 无更改

--- src/camera/src/test_node.cpp
+++ src/camera/src/test_node.cpp
@@ -5,23 +5,23 @@
 #include "mindvision.hpp"
 
 
-// int main() {
-// 	auto mv = MindVision();
-// 	mv.init(2);
+// // int main() {
+// // 	auto mv = MindVision();
+// // 	mv.init(2);
 
-// 	while(1) {
-// 		cv::Mat frame = mv.getFrame();
-// 		if(!frame.empty()) {
-// 			// 显示图像
-// 			cv::imshow("Camera Frame", frame);
-// 			cv::waitKey(1);
-// 		} else {
-// 			printf("Failed to capture image.\n");
-// 			break;
-// 		}
-// 	}
-// 	return 0;
-// }
+// // 	while(1) {
+// // 		cv::Mat frame = mv.getFrame();
+// // 		if(!frame.empty()) {
+// // 			// 显示图像
+// // 			cv::imshow("Camera Frame", frame);
+// // 			cv::waitKey(1);
+// // 		} else {
+// // 			printf("Failed to capture image.\n");
+// // 			break;
+// // 		}
+// // 	}
+// // 	return 0;
+// // }
 
 
 int main() {
