8a068749e9662342029700f7728691ac40809440
Interweave05 <interweave@qq.com>
2024-11-27 19:00:03 +0800

    feat: 测试openvino安装

--- src/aim_armor/src/test/test1.cpp
+++ src/aim_armor/src/test/test1.cpp
@@ -1,3 +1,14 @@
-int main(){
-    return 0;
+#include <iostream>
+#include "openvino/openvino.hpp"
+
+int main() {
+    try {
+        // 获取并输出 OpenVINO 版本信息
+        std::cout << "OpenVINO version: " << ov::get_openvino_version() << std::endl;
+    } catch (const std::exception& ex) {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
