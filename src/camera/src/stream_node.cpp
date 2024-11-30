f1b81ac782883f6286e14783d73f32b63deb64a7
Interweave05 <interweave@qq.com>
2024-11-30 14:09:51 +0800

    feat: 为Hik相机添加使用逻辑

--- src/camera/src/stream_node.cpp
+++ src/camera/src/stream_node.cpp
@@ -4,41 +4,54 @@
 #include <cv_bridge/cv_bridge.hpp>
 
 #include "mindvision.hpp"
+#include "toml.hpp"
 
-class stream_node: public rclcpp::Node {
+class StreamNode: public rclcpp::Node {
 public:
-	stream_node(): Node("stream_node") {
-		camera_.init(2);
+	StreamNode(): Node("stream_node") {
+		if(camera_version == "MV"){
+			camera_.init(2);
+		}	
+		else if(camera_version == "HK"){
+			;
+		}
 
 		publisher_ =
 		    this->create_publisher<sensor_msgs::msg::Image>(
 		        "camera/stream", 10);
 		timer_ = this->create_wall_timer(
 		    std::chrono::milliseconds(0),
-		    std::bind(&stream_node::publish, this));
+		    std::bind(&StreamNode::publish, this));
 	}
 
 private:
 	void publish() {
-		cv::Mat frame = camera_.getFrame();
+		if(camera_version == "MV"){
+			frame = camera_.getFrame();
+		}
+		else if(camera_version == "HK"){
+			;
+		}
+		else{
+			frame = camera_.getFrame();
+		}
 		if(!frame.empty()) {
-			auto msg =
-			    cv_bridge::CvImage(std_msgs::msg::Header(),
-			                       "bgr8", frame)
-			        .toImageMsg();
+			auto msg = cv_bridge::CvImage(std_msgs::msg::Header(),"bgr8", frame).toImageMsg();
 			publisher_->publish(*msg);
 		}
 	}
 
-	MindVision camera_;
-	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr
-	    publisher_;
+	toml::table config = toml::parse_file("./assets/config.toml");
+	std::string camera_version = config["camera"]["version"].value_or("MV");
+	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
 	rclcpp::TimerBase::SharedPtr timer_;
+	MindVision camera_;
+	cv::Mat frame;
 };
 
 int main(int argc, char **argv) {
 	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<stream_node>());
+	rclcpp::spin(std::make_shared<StreamNode>());
 	rclcpp::shutdown();
 	return 0;
 }
