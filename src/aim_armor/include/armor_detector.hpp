5b48244bfc150a3fba97943182124fc40629036b
EiEddie <eieddie4u@outlook.com>
2024-12-06 20:03:32 +0800

    feat: 透视变换

--- src/aim_armor/include/armor_detector.hpp
+++ src/aim_armor/include/armor_detector.hpp
@@ -1,10 +1,14 @@
 #ifndef _AIMARMOR_H_ARMORDETECTOR_
 #define _AIMARMOR_H_ARMORDETECTOR_
 
+#include <opencv2/core/mat.hpp>
 #include <opencv2/opencv.hpp>
 #include <openvino/openvino.hpp>
 #include <toml.hpp>
 #include <string>
+#include <utility>
+
+#include "lights.hpp"
 
 
 class ArmorDetector {
@@ -16,7 +20,7 @@ public:
 	/**
 	 * @brief 初始化装甲板检测类
 	 *
-	 * @retval 1 初始化成功
+	 * @retval 1  初始化成功
 	 * @retval -1 初始化失败
 	 */
 	int init();
@@ -27,12 +31,57 @@ public:
 	 * @param image 输入用于分类的图像
 	 * @return 数据类别
 	 */
-	std::string classify(cv::Mat image);
+	std::string classify(const cv::Mat& image);
+
+private:
+	/**
+	 * @brief 按顺序排列两个灯条的 4 个端点
+	 *
+	 * 按 (左下:-- , 左上:-+ , 右上:++ , 右下:+-) 的顺序排列
+	 *
+	 * @param l1 第一个灯条
+	 * @param l2 第二个灯条
+	 * @return 按顺序排列的点的坐标
+	 */
+	std::vector<cv::Point2d> sort_points(const Light& l1, const Light& l2);
+
+	/**
+	 * @brief 计算原始矩形的信息
+	 *
+	 * 所谓原始矩形, 是指这样一个在空间中的矩形: 它的 4 个顶点被拍摄为照片后与输入的 4 点一一对应.
+	 *
+	 * 计算前会根据矫正相机畸变.
+	 *
+	 * @warning `kpnts` 数组的长度必须等于 4
+	 *
+	 * @param kpnts 矩形四点在图中的坐标
+	 *              必须按顺序排列.
+	 *              参见 `ArmorDetector::resort_points`
+	 * @return 返回一个元组, 内容是: (长宽比, 两边夹角)
+	 */
+	std::pair<float, float> rect_info(const std::vector<cv::Point2d>& kpnts,
+	                                  const Mat3d& camera);
+	// TODO: dist 参数
+	// TODO: camera 和 dist 应储存在类中, 还是全局保存?
+
+	/**
+	 * @brief 透视变换并二值化
+	 *
+	 * @warning `kpnts` 数组的长度必须等于 4
+	 *
+	 * @param img   要处理的图片
+	 * @param out   经过透视变换与二值化后的图片
+	 * @param kpnts 要变换区域的 4 个顶点.
+	 *              必须按顺序排列.
+	 *              参见 `ArmorDetector::resort_points`
+	 * @param size  输出图片的尺寸
+	 */
+	void perspective(const cv::Mat& img, cv::Mat& out,
+	                 const std::vector<cv::Point2d>& kpnts, int size);
 
 private:
 	/// @brief 初始化配置文件
-	toml::table config =
-	    toml::parse_file("./assets/config.toml");
+	toml::table config = toml::parse_file("./assets/config.toml");
 	/// @brief OpenVINO
 	ov::Core core;
 	ov::InferRequest infer_request;
--- src/aim_armor/src/armor_detector.cpp
+++ src/aim_armor/src/armor_detector.cpp
@@ -1,5 +1,9 @@
 #include "armor_detector.hpp"
 
+#include <opencv2/core/matx.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc.hpp>
+
 
 ArmorDetector::ArmorDetector() {
 	printf("ArmorDetector Start\n");
@@ -12,18 +16,14 @@ ArmorDetector::~ArmorDetector() {
 int ArmorDetector::init() {
 	try {
 		// 读取配置
-		std::string model_path =
-		    config["aim_aromr"]["model_path"].value_or(
-		        "./assets/model/best-8.onnx");
-		auto classes_array =
-		    config["aim_aromr"]["classes"].as_array();
+		std::string model_path = config["aim_aromr"]["model_path"].value_or(
+		    "./assets/model/best-8.onnx");
+		auto classes_array = config["aim_aromr"]["classes"].as_array();
 
 		// 初始化模型
 		auto model = core.read_model(model_path);
-		auto compiled_model =
-		    core.compile_model(model, "CPU");
-		infer_request =
-		    compiled_model.create_infer_request();
+		auto compiled_model = core.compile_model(model, "CPU");
+		infer_request = compiled_model.create_infer_request();
 
 		// 读取图像分类类别
 		size_t index = 0;
@@ -35,24 +35,22 @@ int ArmorDetector::init() {
 		}
 		return 1;
 	} catch(const toml::parse_error& ex) {
-		std::cerr << "Error parsing TOML file: "
-		          << ex.what() << std::endl;
+		std::cerr << "Error parsing TOML file: " << ex.what() << std::endl;
 		return -1;
 	} catch(const std::exception& ex) {
-		std::cerr << "Error initializing ArmorDetector: "
-		          << ex.what() << std::endl;
+		std::cerr << "Error initializing ArmorDetector: " << ex.what()
+		          << std::endl;
 		return -1;
 	}
 }
 
-std::string ArmorDetector::classify(cv::Mat image) {
+std::string ArmorDetector::classify(const cv::Mat& image) {
 
 	cv::resize(image, image, cv::Size(64, 64));
 	image.convertTo(image, CV_32F, 1.0 / 255.0); // 归一化
 	// 转换图像格式为 Tensor
 	ov::Shape input_shape = {1, 64, 64, 3}; // NHWC
-	ov::Tensor input_tensor(ov::element::f32, input_shape,
-	                        image.data);
+	ov::Tensor input_tensor(ov::element::f32, input_shape, image.data);
 	infer_request.set_input_tensor(input_tensor);
 
 	// 推理并获取结果
@@ -61,8 +59,84 @@ std::string ArmorDetector::classify(cv::Mat image) {
 	auto output_data = output_tensor.data<float>();
 	int predicted_class = std::distance(
 	    output_data,
-	    std::max_element(
-	        output_data,
-	        output_data + output_tensor.get_size()));
+	    std::max_element(output_data, output_data + output_tensor.get_size()));
 	return classes[predicted_class];
+}
+
+/**
+ * @brief 计算 2 维向量的外积, 返回一个标量
+ */
+template<typename T>
+T pcross(cv::Point_<T> x, cv::Point_<T> y) {
+	return x.x * y.y - x.y * y.x;
+}
+
+/**
+ * @brief 计算 2 维向量的外积, 返回一个标量
+ */
+template<typename T>
+T vcross(cv::Vec<T, 2> x, cv::Vec<T, 2> y) {
+	return x[0] * y[1] - x[1] * y[0];
+}
+
+/**
+ * @brief 计算 4 点的中点
+ *
+ * 此处所指的中点, 为两组对角线的交点
+ *
+ * @warning `pnts` 数组的长度必须等于 4
+ */
+template<typename T>
+cv::Point_<T> pcenter(const cv::Point_<T> pnts[]) {
+	auto p0 = pnts[0];
+	T mu = pcross(pnts[3] - p0, pnts[1] - p0)
+	    / pcross(pnts[2] - p0, pnts[1] - pnts[3]);
+	return p0 + mu * pnts[2];
+}
+
+std::vector<cv::Point2d> ArmorDetector::sort_points(const Light& l1,
+                                                    const Light& l2) {
+	cv::Point2d light_pnts[2][2] = {{l1.pos + l1.offset, l1.pos - l1.offset},
+	                                {l2.pos + l2.offset, l2.pos - l2.offset}};
+	std::vector<cv::Point2d> sorted_pnts(4);
+
+	// 求得四点的重心, 即对四点取平均
+	cv::Point2d center = {0, 0};
+	for(int i = 0; i < 4; i++)
+		center += *((cv::Point2d*)light_pnts + i);
+	center /= 4;
+
+	for(int i = 0; i <= 1; i++) {
+		auto light_pnt = light_pnts[i];
+		auto pos = (light_pnt[0] + light_pnt[1]) / 2. - center;
+
+		int a = pos.x > 0;
+		int b = pcross(pos, light_pnt[0] - center) < 0;
+		sorted_pnts[(a << 1) + b] = light_pnt[0];
+		sorted_pnts[(a << 1) + !b] = light_pnt[1];
+	}
+
+	return sorted_pnts;
+}
+
+void ArmorDetector::perspective(const cv::Mat& img, cv::Mat& out,
+                                const std::vector<cv::Point2d>& kpnts,
+                                int size) {
+	double s = (double)size;
+	std::vector<cv::Point2d> out_pnts = {{0., 0.}, {0., s}, {s, s}, {s, 0.}};
+	auto P = cv::getPerspectiveTransform(kpnts, out_pnts);
+
+	double scale = 1.8;
+	// clang-format off
+	cv::Matx33d Cut = {scale, 0, (1-scale)*s/2,
+	                       0, 1,             0,
+	                       0, 0,             1};
+	// clang-format on
+
+	Mat number_img;
+	cv::warpPerspective(img, number_img, Cut * P, cv::Point(size, size));
+	Mat gray_number_img;
+	cv::cvtColor(number_img, gray_number_img, cv::COLOR_BGR2GRAY);
+	cv::threshold(gray_number_img, out, 0, 255,
+	              cv::THRESH_OTSU | cv::THRESH_BINARY);
 }
