756d76af4dd34ce713a22b3c465afbfa20fb55c4
Interweave05 <interweave@qq.com>
2024-12-19 14:18:40 +0800

    feat: 完善海康相机支持

--- src/camera/src/stream_node.cpp
+++ src/camera/src/stream_node.cpp
@@ -1,9 +1,11 @@
+#include <opencv2/highgui.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 
 #include "mindvision.hpp"
+#include "hikvision.hpp"
 #include "toml.hpp"
 
 class StreamNode: public rclcpp::Node {
@@ -14,10 +16,11 @@ public:
         while (true) {
             try {
                 if (camera_version == "MV") {
-                    camera_.init(2);
+                    camera_mv.init(2);
                     break;
                 } else if (camera_version == "HK") {
-
+                    nRet = camera_hk.init();
+                    RCLCPP_ERROR(this->get_logger(), "Error code: 0x%x", nRet);
                     break;
                 } else {
                     std::printf("配置文件错误，无法识别的相机类型: %s，尝试重新获取配置\n", camera_version.c_str());
@@ -37,13 +40,20 @@ public:
 private:
     void publish() {
         if (camera_version == "MV") {
-            frame = camera_.getFrame();
+            frame = camera_mv.getFrame();
         } else if (camera_version == "HK") {
-            ;
+            result = camera_hk.getFrame();
+            if(result.second == MV_OK){
+                frame = result.first;
+            }
+            else{
+                frame = result.first;
+                RCLCPP_ERROR(this->get_logger(), "Error code: 0x%x", result.second);
+            }
         } else {
-            frame = camera_.getFrame();
+            frame = camera_mv.getFrame();
         }
-
+        cv::imshow("123",frame);
         if (!frame.empty()) {
             auto msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
             publisher_->publish(*msg);
@@ -54,8 +64,11 @@ private:
     std::string camera_version;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
-    MindVision camera_;
+    MindVision camera_mv;
+    HikVision camera_hk;
     cv::Mat frame;
+    std::pair<cv::Mat, int> result;
+    int nRet;
 };
 
 int main(int argc, char **argv) {
