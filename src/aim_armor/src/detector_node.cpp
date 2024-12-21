1bf0b1e673d2f16d5e2080c17821d7f0bcc4009b
Interweave05 <interweave@qq.com>
2024-12-21 17:38:43 +0800

    refactor: 重构装甲板检测框架

--- src/aim_armor/src/detector_node.cpp
+++ src/aim_armor/src/detector_node.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/core/matx.hpp>
 #include <opencv2/opencv.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
@@ -6,10 +7,8 @@
 #include <interfaces/msg/armor.hpp>
 #include <interfaces/msg/armors.hpp>
 #include <geometry_msgs/msg/pose.hpp>
-#include <geometry_msgs/msg/point.hpp>
-
-
 #include "armor_detector.hpp"
+#include "armors.hpp"
 #include "lights.hpp"
 
 
@@ -18,65 +17,71 @@ using ArmorsMsg = interfaces::msg::Armors;
 using ArmorMsg = interfaces::msg::Armor;
 
 
-class DetectorNode: public rclcpp::Node {
+class Detector {
 public:
-	DetectorNode(): Node("detector_node") {
-		subscription_ = this->create_subscription<ImageMsg>(
-		    "camera/stream", 10, std::bind(&DetectorNode::detector, this, std::placeholders::_1));
+	Detector(){};
+	~Detector(){};
 
-		armors_publisher_ =
-		    this->create_publisher<ArmorsMsg>("detector/armors", 10);
-		armorDetector.init();
+	cv::Mat frame;
+	std::vector<ArmorMsg> armors;
+
+	std::vector<ArmorMsg> get_armors() {
+		// test
+		// armors.clear();
+		// armor_msg.number = "1";
+		// armor_msg.distance_to_image_center = .0;
+		// armors.push_back(armor_msg);
+		
+		return armors;
 	}
-	
+
+private:
+	ArmorMsg armor_msg;
+};
+
+
+class Detector_Sender_Node: public rclcpp::Node {
+public:
+	Detector_Sender_Node(Detector &detector):
+	Node("detector_sender_node"), detector_(detector) {
+		subscription_ = this->create_subscription<ImageMsg>(
+		    "camera/stream", 10,
+		    std::bind(&Detector_Sender_Node::detector, this,
+		              std::placeholders::_1));
+		armors_publisher_ =
+		    this->create_publisher<ArmorsMsg>("/detector/armors", 10);
+	}
 
 private:
 	void detector(const ImageMsg::SharedPtr msg) {
 		try {
-			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-			frame = cv_ptr->image;
+			cv_ptr =
+			    cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+			detector_.frame = cv_ptr->image;
 		} catch(cv_bridge::Exception &e) {
 			RCLCPP_ERROR(this->get_logger(),
 			             "Could not convert from '%s' to 'bgr8'.",
 			             msg->encoding.c_str());
 			return;
 		}
-
-		light.from_contour(contour, frame);
-		std::cout << contour << std::endl;
-
-		std::vector<ArmorMsg> armors;
-		for(int i = 0; i < 3; ++i) {
-			ArmorMsg armor;
-			armor.number = "1";
-			armor.type = "BIG";
-			armor.distance_to_image_center = 100.0f;
-			armors.push_back(armor);
-		}
-
-		ArmorsMsg armors_msg;
+		armors_msg.armors = detector_.get_armors();
+		armors_msg.header.frame_id = "1";
 		armors_msg.header.stamp = this->get_clock()->now();
-		armors_msg.header.frame_id = "armors_msg";
-		armors_msg.armors = armors;
-
 		armors_publisher_->publish(armors_msg);
 	}
 
-	// 初始化armordetector
-	ArmorDetector armorDetector;
-	//初始化lights
-	Light light;
+	Detector detector_;
+	ArmorsMsg armors_msg;
 	cv_bridge::CvImagePtr cv_ptr;
-	cv::Mat frame;
-	std::vector<cv::Point> contour;
 	rclcpp::Subscription<ImageMsg>::SharedPtr subscription_;
 	rclcpp::Publisher<ArmorsMsg>::SharedPtr armors_publisher_;
 };
 
 
 int main(int argc, char **argv) {
+	Detector detector;
 	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<DetectorNode>());
+	rclcpp::spin(std::make_shared<Detector_Sender_Node>(detector));
 	rclcpp::shutdown();
 	return 0;
 }
